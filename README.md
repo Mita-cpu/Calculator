bd6c43cf5fa093e04830ab3816e43b5183f31672
Mita-cpu <82151864+Mita-cpu@users.noreply.github.com>
2021-06-17 21:14:42 +0200

    Added a function to find odd or even numbers

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Calculator
+# Calculator - JavaScript functions practice
+This is a simple calculator intended to practice the javaScript functions, mainly to:
+* Find if a number is even or odd
---
+++ calculator.js
@@ -0,0 +1,12 @@
+//even and odd num using functions
+
+let number=13
+let answer=(number % 2)
+function evenOrOdd() {
+  if (answer==0) {
+    console.log ("Even");
+  }else {
+    console.log ("Odd");
+  }
+}
+evenOrOdd()
